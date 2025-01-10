bd36f62d8bdf141c93cb8769caf69dfd4f49794a
Tatennen <123176965+Tatennen@users.noreply.github.com>
2025-01-10 02:48:42 -0300

    Update site config

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,8 +38,8 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'José Ramírez Cortés',
-    subtitle: 'Aquí esta mi primer Blog',
+    title: 'Tatennen',
+    subtitle: 'Ciberseguridad',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
@@ -92,8 +92,8 @@ const siteConfig: SiteConfig = {
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: '¡Hola y bienvenido a Mi rincón de la Web!',
+        text: "Mi nombre **José Ramírez Cortés**, Profesional en inicio de carrera en Ciberseguridad, con formación en normativas de seguridad y técnicas de hacking ético. Con habilidades en auditorías de seguridad y creación de políticas de protección, busco aportar a la seguridad informática de las organizaciones. Apasionado por la tecnología y el aprendizaje continuo en un campo dinámico y en constante evolución.<a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
         image: {
             src: '/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
