9c87b73dbbd9ae6d23e9ac5b4ec5ac33723b3773
Tatennen <123176965+Tatennen@users.noreply.github.com>
2025-01-27 03:55:31 -0300

    Site Config Update

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -105,11 +105,11 @@ const siteConfig: SiteConfig = {
             }
         ]
     },
-    subscribe: {
-        title: 'Subscribe to Dante Newsletter',
-        text: 'One update per week. All the latest posts directly in your inbox.',
-        formUrl: '#'
-    },
+   // subscribe: {
+    //    title: 'Subscribe to Dante Newsletter',
+      //  text: 'One update per week. All the latest posts directly in your inbox.',
+    //    formUrl: '#'
+    // },
     postsPerPage: 8,
     projectsPerPage: 8
 };
